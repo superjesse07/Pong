900a89e80a0c6457c0c21e73966c562e6620e7fb
superjesse07 <jesse@jessar.nl>
2020-09-16 21:23:02 +0200

    init

---
+++ Pong2/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong2
+{
+    public abstract class GameObject
+    {
+        protected Texture2D texture;
+        public Vector2 position;
+
+        public GameObject(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        public abstract void Update(float deltaTime);
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 textureCenter = new Vector2(texture.Width, texture.Height) / 2f;
+            spriteBatch.Draw(texture, position, null, Color.White, 0, textureCenter, 1, SpriteEffects.None, 0) ;
+        }
+
+        public Rectangle GetBounds()
+        {
+            Rectangle bounds = texture.Bounds;
+            bounds.Offset(position - new Vector2(texture.Width,texture.Height)/2f);
+            return bounds;
+        }
+    }
+}
