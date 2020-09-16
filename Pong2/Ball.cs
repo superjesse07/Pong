3422be59dfcafea4d31237772289529e6b762056
superjesse07 <jesse@jessar.nl>
2020-09-16 20:52:22 +0200

    Init

---
+++ Pong2/Ball.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Pong2
+{
+    public class Ball : GameObject
+    {
+        public Vector2 direction; // the direction of the ball
+        public float speed; // the speed of the ball
+        const float maxSpeed = 650; // after years of experimentation this seemed to be a nice value
+        const float speedGrowth = 30;
+        Game1 game;
+
+        public Ball(Game1 game,Texture2D texture, Vector2 position, Vector2 direction, float speed) : base(texture, position)
+        {
+            this.game = game; // this is the game
+            this.speed = speed;
+            this.direction = direction;
+            this.direction.Normalize();
+        }
+        public override void Update(float deltaTime)
+        {
+            position += direction * speed *  deltaTime;
+            float heightOffset = texture.Height / 2f;
+            if (position.Y > Game1.windowSize.Y-heightOffset || position.Y < heightOffset)
+            {
+                direction *= new Vector2(1, -1);
+            }
+            if (position.X > Game1.windowSize.X)
+            {
+                game.PointScored(true);
+            }
+            if(position.X < 0)
+            {
+                game.PointScored(false);
+            }
+        }
+
+        public void OnCollide()
+        {
+            direction *= new Vector2(-1, 1);
+            speed += speedGrowth * (1 - speed /maxSpeed);
+        }
+    }
+}
